ac72049fbd02f5afb65037906d0e18be0b625a8d
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-16 22:46:38 +0300

    style: setup Roboto font & global styles (index.css)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Root component:
 import App from './App';
+// Styles:
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
