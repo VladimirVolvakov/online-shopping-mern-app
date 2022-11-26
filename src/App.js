fe70e68e9728280b1ff4df6cecbe87e8e904b127
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-26 20:40:38 +0300

    feat: add ProductItemPage (page), import Navbar, Offer, Newsletter & Footer components to ProductItemPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 // Pages:
 import Home from "./pages/Home";
 import ProductCatalog from "./pages/ProductCatalog";
+import ProductItemPage from "./pages/ProductItemPage";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
-      <ProductCatalog />
+      {/* <ProductCatalog /> */}
+      <ProductItemPage />
     </div>
   );
 }
---
+++ src/pages/ProductItemPage.jsx
@@ -0,0 +1,18 @@
+// Components:
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Offer from "../components/Offer";
+
+const ProductItemPage = () => {
+  return (
+    <div>
+      <Offer />
+      <Navbar />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+};
+
+export default ProductItemPage;
