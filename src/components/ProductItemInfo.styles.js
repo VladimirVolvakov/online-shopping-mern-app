8671e5cdb0babee2b7e995c15d8248ad0501bfd9
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-26 21:46:24 +0300

    style: add styles for ProductItemInfo component

--- src/components/ProductItemInfo.styles.js
+++ src/components/ProductItemInfo.styles.js
@@ -1,15 +1,34 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 50px;
+  display: flex;
+`;
 
-export const ImageContainer = styled.div``;
+export const ImageContainer = styled.div`
+  flex: 1;
+`;
 
-export const Image = styled.img``; 
+export const Image = styled.img`
+  width: 100%;
+  object-fit: cover;
+`; 
 
-export const InfoContainer = styled.div``; 
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`; 
 
-export const Title = styled.h2``; 
+export const Title = styled.h2`
+  font-size: 32px;
+  font-weight: 300;
+`; 
 
-export const Description = styled.p``; 
+export const Description = styled.p`
+  margin: 20px 0;
+`; 
 
-export const Price = styled.span``;
+export const Price = styled.span`
+  font-size: 32px;
+  font-weight: 100;
+`;
