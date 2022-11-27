6ae2f587e482c617434dc14c359bfbefa5d7a374
VladimirVolvakov <dr.volvakov@gmail.com>
2022-11-27 15:23:08 +0300

    feat: add markup for SignUp component

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,44 @@
 // Styles:
-import {} from "./SignUp.styles";
+import { Container, Wrapper, Title, Form, InputContainer, Label, Input, Agreement, Button } from "./SignUp.styles";
 
 const SignUp = () => {
   return (
-    <div>
-      Register
-    </div>
+    <Container>
+      <Wrapper>
+        <Title>Create a FREE account</Title>
+        <Form>
+          <InputContainer>
+            <Label>Name</Label>
+            <Input placeholder="Ivan" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Last name</Label>
+            <Input placeholder="Ivanov" />
+          </InputContainer>
+          <InputContainer>
+            <Label>E-mail</Label>
+            <Input placeholder="myemail@example.com" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Phone number</Label>
+            <Input placeholder="+7-749-123-45-67" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Password</Label>
+            <Input placeholder="min. 8 signs (a-z, A-Z, 0-9)" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Confirm Password</Label>
+            <Input placeholder="Enter your password again" />
+          </InputContainer>
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>Sign In</Button>
+        </Form>
+      </Wrapper>
+    </Container>
   );
 };
 
---
+++ src/components/SignUp.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Wrapper = styled.div``;
+
+export const Title = styled.h2``; 
+
+export const Form = styled.form``; 
+
+export const InputContainer = styled.div``; 
+
+export const Label = styled.label``; 
+
+export const Input = styled.input``; 
+
+export const Agreement = styled.span``; 
+
+export const Button = styled.button``;
